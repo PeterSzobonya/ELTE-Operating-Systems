0c63c67f22b11c1fc8e1ab5e0711b65b3abf5e1b
NemesLaszlo <wow.laszlo@gmail.com>
2021-04-15 23:48:25 +0200

    alarcos project init

---
+++ ZH Gyak/Álarcos énekes/vetelkedo.c
@@ -0,0 +1,19 @@
+#include "sys/types.h"
+#include "unistd.h"
+#include "stdlib.h"
+#include "signal.h"
+#include "stdio.h"
+#include "string.h"
+#include "time.h"
+#include "wait.h"
+#include "sys/ipc.h"
+#include "sys/msg.h"
+#include "sys/shm.h"
+#include "sys/sem.h"
+#include "sys/stat.h"
+
+int main(int argc, char **argv)
+{
+    printf("Hello Word!\n");
+    return 0;
+}
